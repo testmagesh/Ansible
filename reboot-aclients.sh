6dc9cfa37bc0b83e1fdb487d564b067780c2a8ab
magesh bakthavachalam <test.magesh@gmail.com>
2021-10-04 18:21:51 +0530

    Create reboot-aclients.sh

---
+++ reboot-aclients.sh
@@ -0,0 +1,5 @@
+#sudo -u jenkins -H sh -c "sudo yum update -y"
+#ssh jenkins@192.168.16.138 "sudo yum update -y"
+#ssh jenkins@192.168.16.138 "sudo /sbin/reboot now"
+ssh jenkins@192.168.16.139 "sudo /sbin/reboot now"
+ssh jenkins@192.168.16.140 "sudo /sbin/reboot now"
