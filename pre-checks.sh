eb279379294d6ac86f0a4c46014ccec5331298fd
magesh bakthavachalam <test.magesh@gmail.com>
2021-10-16 09:14:28 +0530

    Update pre-checks.sh

--- pre-checks.sh
+++ pre-checks.sh
@@ -1,4 +1,4 @@
 #sudo -u jenkins -H sh -c "sudo yum update -y"
 #ssh jenkins@192.168.16.138 "sudo yum update -y"
 #ssh jenkins@192.168.16.138 "sudo /sbin/reboot now"
-ssh jenkins@192.168.16.138 "sudo hostname;uptime;cat /etc/redhat-release;rpm -qa | grep -i kernel"
+ssh jenkins@192.168.16.138 "sudo hostname;uptime;cat /etc/redhat-release;rpm -qa | grep -i kernel;pvs;vgs;lvs"
